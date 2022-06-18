f86d3f989d7b41835f3e3147fa58f77ca723ca0d
Jesús Herrera <jesusmaherrera@gmail.com>
2022-06-18 07:40:25 -0600

    Start with base select component

---
+++ components/Base/Input.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <label
+      class="block text-gray-700 text-sm font-bold mb-2"
+      for="select"
+      v-if="!!label"
+    >
+      {{ label }}
+    </label>
+    <select
+      class="w-full p-2 border-gray-300 rounded-lg shadow shadow-sm"
+      id="select"
+      :value="value" @input="$emit('input', $event)"
+    >
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.id">
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  value: [String, Number],
+  label: {
+    type: String,
+    default: '',
+  },
+  options: {
+    type: Array,
+    default: []
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
 <template>
   <h1>Hello World!</h1>
   <input type="checkbox" class="rounded text-pink-500 " />
+  {{ value }}
+  <BaseInput label="company" v-model="value" :options="[{id: 1, label: 'Audi'}, {id: 2, label: 'Mercedes', }, ]"/>
 </template>
+
+<script setup lang="ts">
+const value = 2
+</script>

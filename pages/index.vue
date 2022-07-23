dcf865da7fa25218e7a04765945052c00750aec8
Jesús Herrera <jesusmaherrera@gmail.com>
2022-07-23 07:50:01 -0600

    Add text area

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,16 @@
   <BaseCheckbox label="company" v-model="booleanValue" />
   {{ booleanValue }}
   <BaseSelect label="company" v-model="value" :options="[{id: 1, label: 'Audi'}, {id: 2, label: 'Mercedes', }, ]"/>
-  <BaseInput label="company" v-model="message" type="text"/> {{ message }}
+  <BaseInput label="company" v-model="message" /> {{ message }}
+  <br>
+  <BaseTextarea rows="5" cols="20" v-model="BaseTextareaValue" /> {{ BaseTextareaValue }}
+
 </template>
 
 <script setup lang="ts">
 const value = ref(2)
 const message = ref('hello')
+const BaseTextareaValue = ref('BaseTextareaValue')
 const booleanValue = ref(true)
 
 </script>
